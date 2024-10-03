f2c341db2c48fad26a6a7d4a84cd6911725cdcb3
Eduard Bardají Puig <ebardaji@gmail.com>
2024-10-03 20:04:45 +0100

    Add types

--- src/request/types/btcMethods.ts
+++ src/request/types/btcMethods.ts
@@ -141,20 +141,35 @@ type Recipient = {
   amount: number;
 };
 
-export type SendTransferParams = {
+export const sendTransferMethodName = 'sendTransfer';
+export const sendTransferParamsSchema = v.object({
   /**
    * Array of recipients to send to.
    * The amount to send to each recipient is in satoshis.
    */
-  recipients: Array<Recipient>;
-};
-type SendTransferResult = {
+  recipients: v.array(
+    v.object({
+      address: v.string(),
+      amount: v.number(),
+    })
+  ),
+});
+export type SendTransferParams = v.InferOutput<typeof sendTransferParamsSchema>;
+export const sendTransferResultSchema = v.object({
   /**
    * The transaction id as a hex-encoded string.
    */
-  txid: string;
-};
-
+  txid: v.string(),
+});
+export type SendTransferResult = v.InferOutput<typeof sendTransferResultSchema>;
+export const sendTransferRequestMessageSchema = v.object({
+  ...rpcRequestMessageSchema.entries,
+  ...v.object({
+    method: v.literal(sendTransferMethodName),
+    params: sendTransferParamsSchema,
+    id: v.string(),
+  }).entries,
+});
 export type SendTransfer = MethodParamsAndResult<SendTransferParams, SendTransferResult>;
 
 export type SignPsbtParams = {
