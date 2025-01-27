623f3d4bfa48bcc963ab8dd5fced9368104e428d
Terence Ng <ngtere@live.com.sg>
2025-01-27 16:01:52 +0800

    keep redundancy

--- src/request/types/btcMethods.ts
+++ src/request/types/btcMethods.ts
@@ -219,6 +219,9 @@ export type GetAccountsParams = v.InferOutput<typeof getAccountsParamsSchema>;
 export const getAccountsResultSchema = v.array(
   v.object({
     ...addressSchema.entries,
+    ...v.object({
+      walletType: walletTypeSchema,
+    }).entries,
   })
 );
 export type GetAccountsResult = v.InferOutput<typeof getAccountsResultSchema>;
