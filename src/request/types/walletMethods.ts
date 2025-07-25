1ee254e8858b2742f2a237246cc5c8e5ff806dc2
Eduard Bardají Puig <ebardaji@gmail.com>
2025-07-25 15:38:45 +0100

    Add change by id

--- src/request/types/walletMethods.ts
+++ src/request/types/walletMethods.ts
@@ -195,6 +195,27 @@ export const changeNetworkRequestMessageSchema = v.object({
 export type ChangeNetworkRequestMessage = v.InferOutput<typeof changeNetworkRequestMessageSchema>;
 export type ChangeNetwork = MethodParamsAndResult<ChangeNetworkParams, ChangeNetworkResult>;
 
+export const changeNetworkByIdMethodName = 'wallet_changeNetworkById';
+export const changeNetworkByIdParamsSchema = v.string();
+export type ChangeNetworkByIdParams = v.InferOutput<typeof changeNetworkByIdParamsSchema>;
+export const changeNetworkByIdResultSchema = v.nullish(v.null());
+export type ChangeNetworkByIdResult = v.InferOutput<typeof changeNetworkByIdResultSchema>;
+export const changeNetworkByIdRequestMessageSchema = v.object({
+  ...rpcRequestMessageSchema.entries,
+  ...v.object({
+    method: v.literal(changeNetworkByIdMethodName),
+    params: changeNetworkByIdParamsSchema,
+    id: v.string(),
+  }).entries,
+});
+export type ChangeNetworkByIdRequestMessage = v.InferOutput<
+  typeof changeNetworkByIdRequestMessageSchema
+>;
+export type ChangeNetworkById = MethodParamsAndResult<
+  ChangeNetworkByIdParams,
+  ChangeNetworkByIdResult
+>;
+
 export const getAccountMethodName = 'wallet_getAccount';
 export const getAccountParamsSchema = v.nullish(v.null());
 export type GetAccountParams = v.InferOutput<typeof getAccountParamsSchema>;
@@ -282,6 +303,6 @@ export const addNetworkRequestMessageSchema = v.object({
   }).entries,
 });
 export type AddNetworkRequestMessage = v.InferOutput<typeof addNetworkRequestMessageSchema>;
-export const addNetworkResultSchema = v.nullish(v.null());
+export const addNetworkResultSchema = v.string();
 export type AddNetworkResult = v.InferOutput<typeof addNetworkResultSchema>;
 export type AddNetwork = MethodParamsAndResult<AddNetworkParams, AddNetworkResult>;
