11b8ed6aba6292b3a1ed6bfbf32da4d199bb6a98
Victor Kirov <victor.kirov@gmail.com>
2025-09-18 14:30:36 +0300

    Clean up

--- src/request/sanitizeRequest.ts
+++ src/request/sanitizeRequest.ts
@@ -7,7 +7,25 @@ export const sanitizeRequest = <Method extends keyof Requests>(
   params: Params<Method>,
   providerVersion: number
 ): { method: Method; params: Params<Method>; overrideResponse?: RpcResult<Method> } => {
-  if (method === 'wallet_connect' && providerVersion < 1) {
+  if (providerVersion < 1) {
+    const v1Sanitized = sanitizeV1Request(method, params);
+    method = v1Sanitized.method;
+    params = v1Sanitized.params;
+  }
+
+  return { method, params };
+};
+
+const sanitizeV1Request = <Method extends keyof Requests>(
+  method: Method,
+  params: Params<Method>
+): { method: Method; params: Params<Method> } => {
+  const filterPurposes = (purposes?: AddressPurpose[]) =>
+    purposes?.filter(
+      (purpose) => purpose !== AddressPurpose.Spark && purpose !== AddressPurpose.Starknet
+    );
+
+  if (method === 'wallet_connect') {
     const typedParams = params as Params<'wallet_connect'>;
 
     if (!typedParams) {
@@ -15,12 +33,26 @@ export const sanitizeRequest = <Method extends keyof Requests>(
     }
 
     const { addresses, ...rest } = typedParams;
+    const overrideParams = {
+      ...rest,
+      addresses: filterPurposes(addresses),
+    } as Params<Method>;
 
-    const sanitizedAddresses = addresses?.filter(
-      (addr) => addr !== AddressPurpose.Spark && addr !== AddressPurpose.Starknet
-    );
+    return { method, params: overrideParams };
+  }
 
-    const overrideParams = { ...rest, addresses: sanitizedAddresses } as Params<Method>;
+  if (method === 'getAccounts') {
+    const typedParams = params as Params<'getAccounts'>;
+    const { purposes, ...rest } = typedParams;
+    const overrideParams = { ...rest, purposes: filterPurposes(purposes) } as Params<Method>;
+
+    return { method, params: overrideParams };
+  }
+
+  if (method === 'getAddresses') {
+    const typedParams = params as Params<'getAddresses'>;
+    const { purposes, ...rest } = typedParams;
+    const overrideParams = { ...rest, purposes: filterPurposes(purposes) } as Params<Method>;
 
     return { method, params: overrideParams };
   }
