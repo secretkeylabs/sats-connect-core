9055873f0c94e0609260bf7a80f1b1b706d24cd0
Shaun <37434846+ShaunLWM@users.noreply.github.com>
2025-11-21 08:52:16 +0800

    feat: add Spark clawback transactions method (#105)

--- src/request/types/sparkMethods/flashnetMethods/index.ts
+++ src/request/types/sparkMethods/flashnetMethods/index.ts
@@ -1,6 +1,7 @@
 export * from './clawbackFunds';
 export * from './executeRouteSwap';
 export * from './executeSwap';
+export * from './getEligibleClawbackTransactions';
 export * from './getJwt';
 export * from './intents';
 export * from './signIntent';
