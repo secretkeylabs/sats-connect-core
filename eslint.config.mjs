4826f31f3fb5bb8af28d6673130aebd42b8eb3d2
Eduard Bardají Puig <ebardaji@gmail.com>
2026-01-30 15:04:48 +0200

    Fix lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,8 +7,9 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser } },
+  // Language
+  { languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
   tseslint.configs.recommended,
 
   // Imports
--- src/request/rpc/objects/namespaces/wallet/shared/index.ts
+++ src/request/rpc/objects/namespaces/wallet/shared/index.ts
@@ -1,16 +1,19 @@
-export { PermissionWithoutClientId, permissionRequestParamsSchema } from './permissions';
+export { permissionRequestParamsSchema } from './permissions';
+export type { PermissionWithoutClientId } from './permissions';
 
 export {
-  AllResolvedNetworks,
-  BitcoinNetworkConfigurationOptions,
-  NetworkConfigurationOptions,
-  SparkNetworkConfigurationOptions,
-  StacksNetworkConfigurationOptions,
-  StarknetNetworkConfigurationOptions,
   allResolvedNetworksSchema,
   bitcoinNetworkConfigurationOptionsSchema,
   networkConfigurationOptionsSchema,
   sparkNetworkConfigurationOptionsSchema,
   stacksNetworkConfigurationOptionsSchema,
   starknetNetworkConfigurationOptionsSchema,
+} from './networks';
+export type {
+  AllResolvedNetworks,
+  BitcoinNetworkConfigurationOptions,
+  NetworkConfigurationOptions,
+  SparkNetworkConfigurationOptions,
+  StacksNetworkConfigurationOptions,
+  StarknetNetworkConfigurationOptions,
 } from './networks';
