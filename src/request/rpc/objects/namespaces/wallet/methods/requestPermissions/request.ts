2b638395187e49631fea4679167c0dbca0a48c96
Eduard Bardají Puig <ebardaji@gmail.com>
2026-02-12 11:59:48 +0200

    Update types

--- src/request/rpc/objects/namespaces/wallet/methods/requestPermissions/request.ts
+++ src/request/rpc/objects/namespaces/wallet/methods/requestPermissions/request.ts
@@ -1,36 +1,9 @@
 import { createRequestSchema } from 'src/request/createRequestSchema';
 import { walletMethods } from 'src/request/methods';
 import * as v from 'valibot';
+import { permissionRequestParamsSchema } from '../../shared/permissions';
 
-// Permission request schemas
-const accountActionsSchema = v.object({
-  read: v.optional(v.boolean()),
-});
-
-const walletActionsSchema = v.object({
-  readNetwork: v.optional(v.boolean()),
-});
-
-const accountPermissionRequestSchema = v.object({
-  type: v.literal('account'),
-  resourceId: v.string(),
-  actions: accountActionsSchema,
-});
-
-const walletPermissionRequestSchema = v.object({
-  type: v.literal('wallet'),
-  resourceId: v.string(),
-  actions: walletActionsSchema,
-});
-
-const permissionRequestParamsSchema = v.variant('type', [
-  accountPermissionRequestSchema,
-  walletPermissionRequestSchema,
-]);
-
-export const walletRequestPermissionsParamsSchema = v.nullish(
-  v.array(permissionRequestParamsSchema)
-);
+export const walletRequestPermissionsParamsSchema = v.array(permissionRequestParamsSchema);
 
 export type WalletRequestPermissionsParams = v.InferOutput<
   typeof walletRequestPermissionsParamsSchema
